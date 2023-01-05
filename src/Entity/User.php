ac10365056f22b556853b0bb8d5afe477c676738
Cedric LOMBARDOT <cedric@donkeycode.com>
2023-01-05 16:17:14 +0100

    Update password only

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -30,7 +30,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    public ?string $plainPassword = null;
+    private ?string $plainPassword = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $firstname = null;
@@ -113,6 +113,22 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setFirstname(?string $firstname): self
     {
         $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        // To force doctrine to send persist / update events
+        // be sure change one column
+        $this->setUpdatedAt(new \DateTime());
 
         return $this;
     }
